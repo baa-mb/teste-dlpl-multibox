146b853a7ac5f59a0a1cd541242adf2a00533a7b
baa <34665120+baa-mb@users.noreply.github.com>
2021-04-02 16:07:41 +0200

    Erste Dateien für MakeCode-Projekt

---
+++ main.ts
@@ -0,0 +1,17 @@
+input.onButtonPressed(Button.A, function () {
+	
+})
+input.onButtonPressed(Button.B, function () {
+	
+})
+for (let Index = 0; Index <= 7; Index++) {
+    DLPLmultibox.setPunkt(
+    Index / 1,
+    Index,
+    0x0000ff,
+    nrMatrix.mitte
+    )
+}
+basic.forever(function () {
+	
+})
